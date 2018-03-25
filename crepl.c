6ae6c34257f4660111894c1bcc953c87285aa4d9
tracer <tracer@tracer.org>
2018-03-25 22:43:28 +0800

    >  compile lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- crepl.c
+++ crepl.c
@@ -45,9 +45,10 @@ int main() {
 		    int n = strlen(op); char name[50];
 			int isf = is_func(op, n, name);
 		
-			char filename[50]; strcpy(filename, "123456");
+			char filename[50]; strcpy(filename, strcat(strdup("template-"), name));
 			printf("%s\n",filename);
 			int fd = mkstemp(filename);
+			printf("fd = %d\n",fd);
 			unlink(filename);	
 			printf("Temporary file created\n");
 			
