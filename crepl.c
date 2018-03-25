9ef743f01c16f45c0709d0a63f9b6bf2aa314b4d
tracer <tracer@tracer.org>
2018-03-25 22:47:50 +0800

    >  compile lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- crepl.c
+++ crepl.c
@@ -45,7 +45,7 @@ int main() {
 		    int n = strlen(op); char name[50];
 			int isf = is_func(op, n, name);
 				
-			char filename[50] = "template-123456";
+			char filename[50]; strcpy(filename, name);
 
 			printf("%s\n",filename);
 			int fd = mkstemp(filename);
