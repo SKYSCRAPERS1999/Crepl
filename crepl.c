6c3f79043f8ce53933516b9cb700ee6231b0ab4f
tracer <tracer@tracer.org>
2018-03-25 18:33:04 +0800

    >  compile lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- crepl.c
+++ crepl.c
@@ -1,3 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <assert.h>
+#include <ctype.h>
+#include <time.h>
+
+#define maxn 1024
+
 int main() {
-  return 0;
+	while (1){
+		putchar('$');
+		char op[maxn];
+		bool ok = fgets(op, maxn - 1, stdin);
+		if (!ok){
+			puts("");
+			continue;
+		}else{
+
+
+		}
+	}
+	return 0;
 }
