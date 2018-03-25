f7b299555a4a7bafe50d5f05e46a7a04e59c1c49
tracer <tracer@tracer.org>
2018-03-25 19:15:02 +0800

    >  compile lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- crepl.c
+++ crepl.c
@@ -12,6 +12,25 @@
 
 #define maxn 1024
 
+int is_func(char* s, int n, char* name){
+	int i, j = 0; bool ok = 0;
+	for (i = 0; i + 3 < n; i++){
+		if (s[i] == 'i' && s[i + 1] == 'n' && s[i + 2] == 't'){
+			ok = 1; break;
+		}
+	}
+	if (ok == 0) return 0;
+	for ( ; i + 1 < n; i++){
+		if (s[i] == '('){
+			while (i + 1 < n && s[i + 1] != ')'){
+				name[j++] = s[++i];
+			}
+			name[j] = '\0';
+			return 1;
+		}
+	}
+	return 0;
+}
 int main() {
 	while (1){
 		printf("$ ");
@@ -21,8 +40,36 @@ int main() {
 			puts("");
 			continue;
 		}else{
+		    int n = strlen(op);
+			FILE* fp = tmpfile();
+			printf("Temporary file created\n");
+			
+			fwrite(op, 1, sizeof(op), fp);
+			int pid = fork();
+			if (pid == -1){
+				perror("fork error!\n");
+			}else if (pid == 0){
+				char name[50];
+				if (is_func(op, n, name)){
+					char* myargs[20];
+					myargs[0] = strdup("gcc");
+					myargs[1] = strdup("-cg");
+					myargs[2] = strdup("-fPIC");
+					myargs[3] = strdup("-o");
+					myargs[4] = strdup(name);
+					execvp(myargs[0], myargs);
 
+				}else{
+					
+				}
 
+			}else{
+				wait(NULL);
+				
+
+			}
+
+			fclose(fp);
 		}
 	}
 	return 0;
