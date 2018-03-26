adeda1877241d2018c67b41500ac290d107659fe
tracer <tracer@tracer.org>
2018-03-26 15:03:49 +0800

    >  compile lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- crepl.c
+++ crepl.c
@@ -89,8 +89,9 @@ int main() {
 					exit(1);
 				}
 			}else{
-				rand_str(func_name, 5);	strcat(func_name, "_"); strcat(func_name, name);
+				rand_str(func_name, 5);	
 				printf("%s\n", func_name);	
+				strcat(func_name, "_"); strcat(func_name, name);
 				if (write(fd, func_name, strlen(func_name)) == -1 
 					|| write(fd, "(){return ", 10) == -1
 					|| write(fd, op, strlen(op)) == -1
